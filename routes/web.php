fe2294ff78c8a58fd20412471cf1441124a26081
humamafif <afifalmahbubi04@gmail.com>
2025-06-11 21:27:47 +0700

    :star:new/new routes for register & welcome page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+
+Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+Route::post('/register', [RegisterController::class, 'register']);
+
+Route::get('/products', [WelcomeController::class, 'showAllProducts'])->name('products.index');
+
+Route::get('/products/{id}', [WelcomeController::class, 'showProduct'])->name('products.show');
