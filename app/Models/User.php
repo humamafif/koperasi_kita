4ef8f600aa4aa4955ae8aa49f368ad0b369ab3d8
humamafif <afifalmahbubi04@gmail.com>
2025-06-22 02:29:19 +0700

    :bug:fix/override canAccesPanel() for allow user access dashboard

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, HasRoles;
@@ -36,6 +37,11 @@ class User extends Authenticatable
         'is_anggota_tetap' => 'boolean',
         'tanggal_menjadi_anggota_tetap' => 'date',
     ];
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return true;
+    }
 
     /**
      * The attributes that should be hidden for serialization.
