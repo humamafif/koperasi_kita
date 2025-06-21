e3c1177352c04889f0fffa4e0ab73b33288bf245
humamafif <afifalmahbubi04@gmail.com>
2025-06-22 02:30:04 +0700

    :up:update/add registration

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->registration()
             ->globalSearch(false)
             ->login()
             ->colors([
