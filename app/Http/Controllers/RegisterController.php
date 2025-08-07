a853cf210b3e1ffb3d287ae12643770900aa1b0b
humamafif <afifalmahbubi04@gmail.com>
2025-08-07 10:25:21 +0700

    :bug:fix/fix auto login after register

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Models\Role;
 
 class RegisterController extends Controller
@@ -31,8 +32,9 @@ class RegisterController extends Controller
         $role = Role::findByName('anggota');
         $user->assignRole($role);
         event(new Registered($user));
+        Auth::login($user);
 
-        return redirect()->route('filament.anggota.auth.login')
+        return redirect()->route('filament.anggota.pages.dashboard')
             ->with('status', 'Akun berhasil dibuat! Silakan login untuk melanjutkan.');
     }
 }
